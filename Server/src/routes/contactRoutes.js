9616dd9918cdf6a9d225b305a1aab95b38b575ab
Varun Vaibhav <varun7058@gmail.com>
2026-01-26 15:28:22 +0530

    Delete Server directory

--- Server/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { submitMessage } from '../controllers/contactController.js';
-
-const router = express.Router();
-
-router.post('/submit', submitMessage);
-
-export default router;
